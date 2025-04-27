02b5c9c926828974ff222829aebf8ffcd7acc9af
Santiago Ledesma <ledesmasantiago10@gmail.com>
2025-04-27 15:13:16 -0300

    Modificación de Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import '../styles/Footer.css';
+import React from "react";
+import "../styles/Footer.css";
 
 const Footer = () => (
   <footer className="footer">
-    <p>Grupo 7 - Comisión 1 - Programaciòn III</p>
+    <div className="footer-content">
+      <p>Grupo 7 - Comisión 1 - Programación III</p>
+    </div>
+    <p className="footer-note">© 2025 Todos los derechos reservados.</p>
   </footer>
 );
 
 export default Footer;
+
